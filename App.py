afc6526f198cd505b0a9fe3d6bb7020afeb181bc
matiassfunes <matiassfunes@gmail.com>
2021-01-18 17:59:45 -0300

    commit 2

--- App.py
+++ App.py
@@ -33,13 +33,39 @@ def add_contact():
         flash('Contacto agregado')
         return redirect(url_for('Index'))
 
-@app.route('/edit')
-def edit_contact():
-    return 'edit contact'
+@app.route('/edit/<id>')
+def get_contact(id):
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM contactos WHERE id = %s', (id))
+    data = cur.fetchall()
+    return render_template('edit_contact.html', contacto = data[0])
 
-@app.route('/delete')
-def delete_contact():
-    return 'delete contact'
+@app.route('/update/<id>', methods=["POST"])
+def update_contact(id):
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        phone = request.form['phone']
+        email = request.form['email']
+        cur = mysql.connection.cursor()
+        cur.execute("""
+            UPDATE contactos
+            SET fullname = %s,
+                phone = %s,
+                email = %s
+            WHERE id = %s
+        """, (fullname, phone, email, id))
+        mysql.connection.commit()
+        flash('Contacto actualizado')
+        return redirect(url_for('Index'))
+
+@app.route('/delete/<string:id>')
+def delete_contact(id):
+    cur = mysql.connection.cursor()
+    cur.execute('DELETE FROM contactos WHERE id = {0}'.format(id))
+    mysql.connection.commit()
+    flash('Contacto eliminado ')
+    return redirect(url_for('Index'))
+    return id
 
 if __name__ == '__main__':
     app.run(debug = True)
